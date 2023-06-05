9963335558d2f8914c7f14d52d6169a700d615c7
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-06-05 19:41:39 +0300

    EasyCashIdentity

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -32,5 +32,30 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             appUserEditDto.ImageUrl = values.ImageUrl;
             return View(appUserEditDto);
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserEditDto appUserEditDto)
+        {
+            if (appUserEditDto.Password == appUserEditDto.ConfirmPassword)
+            {
+
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.Surname = appUserEditDto.Surname;
+                user.City = appUserEditDto.City;
+                user.District = appUserEditDto.District;
+                user.Name = appUserEditDto.Name;
+                user.ImageUrl = "test";
+                user.Email = appUserEditDto.Email;
+                //Güncellediğimiz şifreyi veri tabınına yine şifreli olarak göreceğiz.
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, appUserEditDto.Password);
+                //UpdateAsync var olan kullanıcı üzerinde işlem gerçekleştiriyor.
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+        }
     }
 }
